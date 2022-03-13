0d3dc32916bb26aaa337cf8f0f9e2b8dc9f84429
Wildan Setya Nugraha <96909507+wwdnn@users.noreply.github.com>
2022-03-13 22:29:22 +0700

    Memperbaiki tulisan

--- save_file.c
+++ save_file.c
@@ -3,18 +3,16 @@
 
 void save_file(char FileName[30], char data[200]){
 	FILE *File;
-
-	File = fopen(FileName, "w");
-    if(File==NULL)
-        printf("ERROR FILE TIDAK ADA");
-    else
-    {
-        while(strlen(data)>0)
-        {
-            fputs(data, File);
+	File = fopen(FileName, "w"); 
+	if(File==NULL){
+		printf("ERROR FILE TIDAK ADA");
+	} else {
+		while(strlen(data)>0){
+		    	fputs(data, File);
 			fputs("\n", File);
-            gets(data);
-        }
-    }
-    fclose(File);
+		    	gets(data);
+		}
+	}
+	
+	fclose(File);
 }
